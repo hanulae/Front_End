8ebb69d272c2f12c4aefb08715bf0fc824a88790
khakisage <jongjin201@gmail.com>
2025-04-27 15:20:32 +0900

    BaseInput 컴포넌트 추가: 텍스트 입력 필드 및 클리어 버튼 기능 구현

---
+++ src/components/common/input/BaseInput.tsx
@@ -0,0 +1,81 @@
+import {
+  StyleSheet,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+// import Typo from '../Typo';
+import BtnClose from '../../../assets/Icon/Icon_BtnCloseGray.svg';
+import {useState} from 'react';
+
+interface IBaseInputProps extends TextInputProps {
+  error?: string;
+  placeholder?: string;
+  clearable?: boolean;
+}
+
+const BaseInput = ({
+  error,
+  clearable = false,
+  value,
+  onChangeText,
+  ...props
+}: IBaseInputProps) => {
+  const [focused, setFocused] = useState(false);
+  const showClear = clearable && focused && !!value;
+  console.log('type', typeof BtnClose);
+  return (
+    <View style={styles.container}>
+      <TextInput
+        {...props}
+        value={value}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onChangeText={onChangeText}
+        placeholder={props.placeholder}
+        style={[styles.input, error && styles.errorInput]}
+      />
+      {showClear && (
+        <TouchableOpacity
+          onPress={() => onChangeText?.('')}
+          style={styles.clearButton}
+          hitSlop={{top: 0, bottom: 0, left: 0, right: 0}}>
+          <BtnClose width={16} height={16} />
+        </TouchableOpacity>
+      )}
+      {/* {error && <Typo style={styles.errorText}>{error}</Typo>} */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 18,
+  },
+  input: {
+    width: '100%',
+    fontSize: 16,
+    borderRadius: 10,
+    fontWeight: '500',
+    paddingVertical: 18,
+    backgroundColor: '#F5F5F5',
+    paddingHorizontal: 20,
+  },
+  errorInput: {},
+  errorText: {},
+  clearButton: {
+    position: 'absolute',
+    right: 12,
+    top: '50%',
+    transform: [{translateY: 6}],
+    padding: 4,
+  },
+});
+
+export default BaseInput;
