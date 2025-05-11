f9825c21be6cd4d1baf5e96d980a46705c782028
khakisage <jongjin201@gmail.com>
2025-05-11 15:41:55 +0900

    Add SVG assets for check icons and bank list
    - Created new SVG files for check icons in various states (active, check, default, disable) for Check01 and Check_03.
    - Added SVG files for Check_Off and Check_On states.
    - Introduced a new TypeScript file (BankList.ts) containing a list of banks with their respective icons.

---
+++ src/constant/BankList.ts
@@ -0,0 +1,29 @@
+import KbIcon from '../assets/Bank/Bank_KB.svg';
+import ShinhanIcon from '../assets/Bank/Bank_SinHan.svg';
+import WooriIcon from '../assets/Bank/Bank_Woori.svg';
+import HanaIcon from '../assets/Bank/Bank_Hana.svg';
+import NHIcon from '../assets/Bank/Bank_NH.svg';
+import KakaobankIcon from '../assets/Bank/Bank_Kakao.svg';
+import CitiIcon from '../assets/Bank/Bank_Citi.svg';
+import PostIcom from '../assets/Bank/Bank_Post.svg';
+import KbankIcon from '../assets/Bank/Bank_Kbank.svg';
+import TossIcom from '../assets/Bank/Bank_Toss.svg';
+import MGIcom from '../assets/Bank/Bank_MG.svg';
+import ShIcon from '../assets/Bank/Bank_Sh.svg';
+import SCIcon from '../assets/Bank/Bank_SC.svg';
+
+export const BankList = [
+  {id: 'kb', name: '국민은행', icon: KbIcon},
+  {id: 'shinhan', name: '신한은행', icon: ShinhanIcon},
+  {id: 'woori', name: '우리은행', icon: WooriIcon},
+  {id: 'hana', name: '하나은행', icon: HanaIcon},
+  {id: 'nh', name: '농협은행', icon: NHIcon},
+  {id: 'kakaobank', name: '카카오뱅크', icon: KakaobankIcon},
+  {id: 'citi', name: '씨티은행', icon: CitiIcon},
+  {id: 'post', name: '우체국', icon: PostIcom},
+  {id: 'kbank', name: '케이뱅크', icon: KbankIcon},
+  {id: 'toss', name: '토스뱅크', icon: TossIcom},
+  {id: 'mg', name: 'MG새마을금고', icon: MGIcom},
+  {id: 'sh', name: 'Sh수협은행', icon: ShIcon},
+  {id: 'sc', name: 'SC제일은행', icon: SCIcon},
+];
